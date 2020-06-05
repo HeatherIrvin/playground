20483d49b492eda49203372e96c90f5fa7785d0b
Heather Irvin <hlirvin@gmail.com>
2020-06-05 02:55:39 +0000

    Added Background Generator Homework

--- UdemyCourse/DOM/script.js
+++ UdemyCourse/DOM/script.js
@@ -102,3 +102,22 @@ addDeleteListener();
 //   }  
 // })
 
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.querySelector("body");
+
+function changeBackground() {
+  body.style.background = "linear-gradient(to right," + color1.value + "," + color2.value + ")";
+  css.textContent = body.style.background + ";";
+}
+
+function setRandomColors() {
+  color1.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+  color2.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+color1.addEventListener("input", changeBackground);
+color2.addEventListener("input", changeBackground);
+setRandomColors();
+changeBackground();
