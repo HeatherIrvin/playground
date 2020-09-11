921bb621cb95467123a6a9c552aa31b52ce1f625
Heather Irvin <hlirvin@gmail.com>
2020-09-11 19:16:32 +0000

    added more adv js

--- UdemyCourse/AdvancedJavascript/script.js
+++ UdemyCourse/AdvancedJavascript/script.js
@@ -253,85 +253,316 @@ cow.playSound();
 
 */
 
-// Solve the below problems:
+// // Solve the below problems:
 
 
-// #1) Check if this array includes the name "John".
-const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
-dragons.includes("John");
-// false
+// // #1) Check if this array includes the name "John".
+// const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+// dragons.includes("John");
+// // false
 
 
-// #2) Check if this array includes any name that has "John" inside of it. If it does, return that
-// name or names in an array.
+// // #2) Check if this array includes any name that has "John" inside of it. If it does, return that
+// // name or names in an array.
 const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
 dragons.filter(name => name.includes('John'))
 // [ 'Johnathan' ]
 
-// #3) Create a function that calulates the power of 100 of a number entered as a parameter
+// // #3) Create a function that calulates the power of 100 of a number entered as a parameter
 
-const power = (x) => x**100;
-power(2);
-power(10000);
+// const power = (x) => x**100;
+// power(2);
+// power(10000);
 
-// #4) Useing your function from #3, put in the paramter 10000. What is the result?
-// Research for yourself why you get this result
+// // #4) Useing your function from #3, put in the paramter 10000. What is the result?
+// // Research for yourself why you get this result
 
-//Infinity
-
-
+// //Infinity
 
 
-// Solve the below problems:
-
-// #1) Line up the Turtle and the Rabbit at the start line:
-const startLine = '     ||<- Start line';
-let turtle = '🐢';
-let rabbit = '🐇';
-
-console.log(startLine);
-console.log(turtle.padStart(9));
-console.log(rabbit.padStart(9));
-
-// it should look like this:
-'     ||<- Start line'
-'       🐢'
-'       🐇'
-
-// when you do:
-console.log(startLine);
-console.log(turtle);
-console.log(rabbit);
 
 
-// #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
-// Read about what the second parameter does in padEnd and padStart
-let turtle = '🐢';
-turtle = turtle.trim().padEnd(9, "=");
-// fills the pad with whatever is in the ""
+// // Solve the below problems:
+
+// // #1) Line up the Turtle and the Rabbit at the start line:
+// const startLine = '     ||<- Start line';
+// let turtle = '🐢';
+// let rabbit = '🐇';
+
+// console.log(startLine);
+// console.log(turtle.padStart(9));
+// console.log(rabbit.padStart(9));
+
+// // it should look like this:
+// '     ||<- Start line'
+// '       🐢'
+// '       🐇'
+
+// // when you do:
+// console.log(startLine);
+// console.log(turtle);
+// console.log(rabbit);
 
 
-// #3) Get the below object to go from:
-let obj = {
-  my: 'name',
-  is: 'Rudolf',
-  the: 'raindeer'
+// // #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
+// // Read about what the second parameter does in padEnd and padStart
+// let turtle = '🐢';
+// turtle = turtle.trim().padEnd(9, "=");
+// // fills the pad with whatever is in the ""
+
+
+// // #3) Get the below object to go from:
+// let obj = {
+//   my: 'name',
+//   is: 'Rudolf',
+//   the: 'raindeer'
+// }
+// // to this:
+// 'my name is Rudolf the raindeer'
+
+// let myThing = "";
+// Object.keys(obj).forEach((key, index)=> {
+//   myThing = myThing + " " + key + " " + obj[key];
+// })
+// console.log(myThing);
+
+// // correct way
+// let obj = {
+//   my: 'name',
+//   is: 'Rudolf',
+//   the: 'raindeer'
+// }
+// Object.entries(obj).map(value => value);
+// Object.entries(obj).map(value => value.join(" "));
+// Object.entries(obj).map(value => value.join(" ")).join(' ');
+
+
+
+
+
+Solve the below questions:
+
+//#1 Turn this array into a new array: [1,2,3,[4],[5]]. Bonus if you can do it on one line
+const array = [[1],[2],[3],[[[4]]],[[[5]]]];
+array.flat(2);
+
+
+// #2 Turn this array into a new array: [ 'Hello young grasshopper!', 'you are', 'learning fast!' ]
+const greeting = [["Hello", "young", "grasshopper!"], ["you", "are"], ["learning", "fast!"]];
+
+const greeting2 = greeting.flatMap(item => item.join(' '));
+console.log(greeting2);
+
+// solution
+greeting.flatMap(x => x.join(' '));
+
+//#3 Turn the greeting array above into a string: 'Hello young grasshopper you are learning fast!'
+
+const greeting = [["Hello", "young", "grasshopper!"], ["you", "are"], ["learning", "fast!"]];
+console.log(greeting.flatMap(x => x.join(' ')).join(' '))
+
+//#4 Turn the trapped 3 number into: [3]
+const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
+
+//Solution
+console.log(trapped.flat(Infinity))
+// Infintiy is actually a LARGE number in JavaScipt. It represents the maximum amount of memory that we can hold for a number! Learn more here: https://riptutorial.com/javascript/example/2337/infinity-and--infinity
+
+//#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
+const userEmail3 = '     cannotfillemailformcorrectly@gmail.com   '
+const userEmail4 = userEmail3.trimStart().trimEnd();
+console.log(userEmail4);
+
+//Solution:
+console.log(userEmail3.trimEnd().trimStart())
+
+//#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+
+//Solution
+const usersArray = Object.entries(users)
+
+console.log(usersArray);
+
+
+//#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
+
+const other = usersArray.map(item => [item[0], item[1] * 2]);
+console.log(other);
+
+//Solution
+updatedUsersArray = usersArray.map((user) => [user[0], user[1] * 2]);
+
+
+// //#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+
+
+// const newUpdatedUsers = Object.fromEntries(updatedUsersArray);
+// console.log(newUpdatedUsers);
+
+// // advance loops
+
+// const basket = ['Apples', 'Oranges','Grapes']
+// //1
+for (let i = 0; i< basket.length; i++) {
+  console.log(basket[i]);
 }
-// to this:
-'my name is Rudolf the raindeer'
 
-let myThing = "";
-Object.keys(obj).forEach((key, index)=> {
-  myThing = myThing + " " + key + " " + obj[key];
-})
-console.log(myThing);
+// //2
+// basket.forEach(item =>{
+//   console.log(item);
+// })
 
-// correct way
-let obj = {
-  my: 'name',
-  is: 'Rudolf',
-  the: 'raindeer'
+// // For of - arrays and strings
+// // iterating
+// for (item of basket) {
+//   console.log(item);
+// }
+
+
+// // For In - Works with objects - see object properties
+// // enumerating
+// const detailedBasket = {
+//   apples: 5,
+//   oranges: 10,
+//   grapes: 15
+// }
+
+// for (item in detailedBasket) {
+//   console.log(item);
+// }
+// HOmework
+
+
+// Question #1:
+// create a function called biggestNumberInArray() that takes
+// an array as a parameter and returns the biggest number.
+// biggestNumberInArray([-1,0,3,100, 99, 2, 99]) should return 100;
+// Use at least 3 different types of javascript loops to write this:
+const array = [-1,0,3,100, 99, 2, 99] // should return 100
+const array2 = ['a', 3, 4, 2] // should return 4
+const array3 = [] // should return 0
+
+function biggestNumberInArray(arr) {
+  let mybig = 0;
+  arr.forEach(item =>{
+    if(item>mybig) {
+      mybig = item;
+    }
+  })
+  console.log(mybig);
 }
-Object.entries(obj).map(value => value);
-Object.entries(obj).map(value => value.join(" "));
-Object.entries(obj).map(value => value.join(" ")).join(' ');
+
+function biggestNumberInArray2(arr) {
+  let mybig = 0;
+  for (item of arr) {
+    if(item>mybig) {
+      mybig = item;
+    }
+  }
+  console.log(mybig);
+}
+
+function biggestNumberInArray3(arr) {
+
+}
+
+biggestNumberInArray(array);
+biggestNumberInArray2(array);
+
+
+// Question #2:
+// Write a function checkBasket() that lets you know if the item is in the basket or not
+amazonBasket = {
+  glasses: 1,
+  books: 2,
+  floss: 100
+}
+
+function checkBasket(basket, lookingFor) {
+  for (item in basket) {
+    if (item === lookingFor) {
+      return `${lookingFor} is in your basket`
+    }
+  }
+  return 'that does not exist in your basket'
+}
+
+checkBasket(amazonBasket, "booddks");
+
+
+// make list of items, pop off random
+
+let arrEquip = ["Sword", "Hammer", "Bow", "Dagger", "Sheild", "Apples"];
+
+function drawEquip(deck) {
+  let eindex = Math.floor( Math.random()*deck.length );
+  console.log( deck[eindex] ); // Log the item
+  deck.splice( eindex, 1 ); // Remove the item from the array
+  console.log(deck);
+}
+
+drawEquip(arrEquip);
+drawEquip(arrEquip);
+drawEquip(arrEquip);
+drawEquip(arrEquip);
+
+// let eindex = Math.floor( Math.random()*arrEquip.length );
+// console.log( arrEquip[eindex] ); // Log the item
+// arrEquip.splice( eindex, 1 ); // Remove the item from the array
+// console.log(arrEquip);
+
+
+// Practice
+
+// Question 1: Clean the room function: given an input of [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20], make a function that organizes these into individual array that is ordered. For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]. Bonus: Make it so it organizes strings differently from number types. i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]]
+
+
+//goal return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]
+let myRoom = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+sortArray(myRoom);
+
+function compareNumbers(a, b) {
+  return a - b;
+}
+
+function sortArray(arr) {
+  arr.sort(compareNumbers); 
+  let i = 1;
+  let oldArray = [...arr];
+  let newArray = [];
+  let dnew = [];
+
+  for (item of arr) {
+    if (item < arr[i]) {
+      for (let c = 0; c < arr.filter(x => x === item).length; c++ ) {
+        newArray.push(item);
+        oldArray.splice(1,1);
+        }
+      dnew.push(newArray);
+    }
+    if (i >= arr.length) {
+          newArray.push(item);
+          oldArray.splice(1,1);
+          dnew.push(newArray);
+        }
+    i++;
+    newArray = [];
+  }
+  
+  console.log(dnew);
+  return arr;
+}
+
+
+
+
+
+
+
+console.log(myRoom);
+
+
+// Question 2: Write a javascript function that takes an array of numbers and a target number. The function should find two different numbers in the array that, when added together, give the target number. For example: answer([1,2,3], 4)should return [1,3]
+
+// Question 3: Write a function that converts HEX to RGB. Then Make that function auto-dect the formats so that if you enter HEX color format it returns RGB and if you enter RGB color format it returns HEX.
